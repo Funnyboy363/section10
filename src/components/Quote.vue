345ed22b53b0a669de1c76be2b3917eef731aefa
Grant <grantshoop@gmail.com>
2019-10-04 21:10:43 -0600

    went ahead and did the whole module

---
+++ src/components/Quote.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-lg-3 text-center">
+<div class="panel panel-default">
+<div class="panel-body quote">
+    <slot></slot>
+</div>
+</div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
+
+<style>
+.panel-body {
+    font-family: cursive;
+    font-size: 24px;
+    color: #6e6e6e;
+    border: 2px solid lightgray;
+    border-radius: 5px;
+    margin: 20px 2px;
+}
+
+.quote {
+    cursor:pointer;
+}
+
+.quote:hover {
+    background-color: #ffe2e2;
+}
+
+
+</style>
